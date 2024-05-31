f9dd29f8930b60c6ebe6f23e199fa5ac354fa7fe
fernandasfarias <fernandafariass734@gmail.com>
2024-05-31 14:00:32 -0300

    commit

--- Mundo 3/Curso Python #16 - Tuplas/exercicio74pt2.py
+++ Mundo 3/Curso Python #16 - Tuplas/exercicio74pt2.py
@@ -1,8 +1,15 @@
+# Importar a biblioteca random para gerar os números aleatórios.
 from random import randint 
+
+# Criar a tupla com os 5 números aleatórios.
 numeros = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+
+# Imprimir cada um desses números na tela.
 print('Os números escolhidos foram: ', end='')
 for num in numeros:
     print(f'{num}', end=' ')
+
+# Encontrar o maior e o menor valor da tupla. 
 maior_num = menor_num = numeros[0]
 for num in numeros[1:]:
     if num > maior_num:
