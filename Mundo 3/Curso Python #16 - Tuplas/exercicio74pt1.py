896119a8c29d314556fc1f48aebd53cb129162a1
Fernanda Silva Farias <fernandafariass734@gmail.com>
2024-05-31 13:59:24 -0300

    Update exercicio74pt1.py

--- Mundo 3/Curso Python #16 - Tuplas/exercicio74pt1.py
+++ Mundo 3/Curso Python #16 - Tuplas/exercicio74pt1.py
@@ -3,5 +3,5 @@ numeros_aleatorios = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1,
 print(f'Os números escolhidos foram:', end=' ')
 for num in numeros_aleatorios:
     print(f'{num}', end=' ')
-print(f'\n O maior número foi: {max(numeros_aleatorios)}')
+print(f'\nO maior número foi: {max(numeros_aleatorios)}')
 print(f'O menor número foi: {min(numeros_aleatorios)}')
